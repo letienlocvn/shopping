fe289c9f2dd981bece8fabfa4e7269b85450fa0c
letienlocvn <letienlocvn@gmail.com>
2022-06-05 09:20:03 +0700

    Setup routes

---
+++ src/components/AddNewUsers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddNewUsers = () => {
+  return <div>AddNewUsers</div>;
+};
+
+export default AddNewUsers;
---
+++ src/routes/Users.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div style={{ display: "block", fontSize: "16px" }}>
+      <h1>Hello Users</h1>
+    </div>
+  );
+};
+
+export default Users;
